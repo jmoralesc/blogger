417077b64aa36417dc2f465233b0cd88d5c51cbe
Crowd Interactive <crowdinteractive@Mac-mini-de-Crowd.local>
2014-05-16 09:53:49 -0500

    Added a few gems

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515224352) do
+ActiveRecord::Schema.define(version: 20140516142637) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20140515224352) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "attachments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "comments", force: true do |t|
